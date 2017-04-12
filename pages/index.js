fbe2722570816c83b9b1df317fc868a63515a90e
Matt Marino <matthewsmarino@gmail.com>
2017-04-12 13:33:45 -0400

    initial hook-up of socket.io

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import reactStamp from 'react-stamp'
+import io from 'socket.io-client'
 
 import Game from '../components/game'
 
-export default () => (
-  <Game />
+const Page = {
+  displayName: 'Page',
+
+  componentDidMount () {
+    const socket = io()
+  },
+
+  render () {
+    return (
+      <Game />
+    )
+  }
+}
+
+export default reactStamp(React).compose(
+  Page
 )
--- server.js
+++ server.js
@@ -2,19 +2,26 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+var server
 
 app.prepare().then(() => {
-  createServer((req, res) => {
+  server = createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
 
     handle(req, res, parsedUrl)
   })
-  .listen(3000, (err) => {
+
+  const io = require('socket.io')(server)
+
+  io.on('connection', (socket) => {
+    console.log('a user connected')
+  })
+
+  server.listen(3000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
